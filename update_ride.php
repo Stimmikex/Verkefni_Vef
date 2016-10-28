3580953c28b2d92d498923deeccb1ee5509aa7f7
Stimmikex <styrmir97@gmail.com>
2016-10-28 14:28:13 +0000

    updated update_ride

--- update_ride.php
+++ update_ride.php
@@ -133,6 +133,99 @@
 				<?php
 					$res = null;
 					}
+				}
+
+					if ($ridetype == 1) {
+						$passQuery = "SELECT asking.id AS id,
+											asking.timewhen AS timewhen,
+											asking.datetogo AS datetogo,
+											asking.message AS message,
+											asking.to_id AS to_id,
+											asking.from_id AS from_id,
+											users.username AS user_id
+										FROM asking
+										INNER JOIN users ON users.id = asking.user_id
+										WHERE asking.id = $ride_id";
+						$toQuery = "SELECT place FROM location WHERE id=:place_id LIMIT 1";
+						$fromQuery = "SELECT place FROM location WHERE id=:place_id LIMIT 1";
+
+						$infoRes = $db->prepare($passQuery);
+						$infoRes->execute();
+
+						/*$Profilequery = "SELECT * FROM users WHERE id = :id";
+					    $imgres = $db->prepare($Profilequery);
+					    $imgres->bindParam(':id', $user_id);
+					    $imgres->execute();
+
+					    $dirname = "img/user_img/";
+					    while ($rowimg = $imgres->fetch(PDO::FETCH_ASSOC)) {
+							echo "<img src=".$dirname. $rowimg['user_image']." class='profile_img'><br>";
+					    }*/
+
+						while ($row = $infoRes->fetch(PDO::FETCH_ASSOC)) {
+							$fromRes = $db->prepare($fromQuery);
+							$fromRes->bindParam(':place_id', $row['from_id']);
+
+							$toRes = $db->prepare($toQuery);
+							$toRes->bindParam(':place_id', $row['to_id']);
+
+							// echo "<div class='ride-info'>";
+
+							$fromRes->execute();
+
+							$query = "SELECT id, place FROM location ORDER BY place ASC";
+			    			$res = $db->prepare($query);
+			    			$res->execute();
+
+		    				echo "<label for='from'>From:*</label>";
+		    				echo "<select name='from' class='browser-default'>";
+
+							while ($row2 = $res->fetch(PDO::FETCH_ASSOC)) {
+
+			    				if ($row2['id'] === $row['from_id']) {
+			    					echo '<option value="'.$row2['id'].'" selected>'.$row2['place'].'</option>';
+			    				} else {
+				    				echo '<option value="'.$row2['id'].'">'.$row2['place'].'</option>';
+			    				}
+							}
+
+			    			echo "</select>";
+
+			    			$res->execute();
+
+			    			echo "<label for='to'>To:*</label>";
+		    				echo "<select name='to' class='browser-default'>";
+
+							while ($row2 = $res->fetch(PDO::FETCH_ASSOC)) {
+
+			    				if ($row2['id'] === $row['to_id']) {
+			    					echo '<option value="'.$row2['id'].'" selected>'.$row2['place'].'</option>';
+			    				} else {
+				    				echo '<option value="'.$row2['id'].'">'.$row2['place'].'</option>';
+			    				}
+							}
+
+			    			echo "</select>";
+
+							$toRes->execute();
+
+						$mainRes = null;
+
+				?>
+				<label for="time">Time:*</label>
+				<input type='text' name='timeride' value="<?php echo $row['timewhen']; ?>" required>
+				<label for="date">Date:*</label>
+				<input type="text" id="datepicker" name="dateride" value="<?php echo $row['datetogo']; ?>" required>
+				<div class="row">
+		        	<div class="input-field">
+		         	<textarea id="ck_editor" name="message"><?php echo $row['message']; ?></textarea>
+		        	<!-- <label for="icon_prefix2">Message: </label> -->
+		       		</div>
+		     	</div>
+				<input type="submit" name="submit" value="Add" id="register-button" class="btn waves-effect waves-light">
+				<?php
+					$res = null;
+					}
 				} 
 				?>
 		</form>
